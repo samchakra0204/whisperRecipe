a0e738a90d751fb3c1293f3ae19546f3c6c5b21c
samchakra0204 <samagchakrabarti@gmailcom>
2025-05-24 19:36:25 +0530

    fix: base to / and vercel.json mime headers

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { componentTagger } from "lovable-tagger";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/whisper-recipe/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8000,
